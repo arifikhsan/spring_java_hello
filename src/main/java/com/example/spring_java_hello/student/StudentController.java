a8ae1a0793faaaaacc7eb5fa9c678e2aee78ecb5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2022-03-01 12:35:12 +0700

    clean import

--- src/main/java/com/example/spring_java_hello/student/StudentController.java
+++ src/main/java/com/example/spring_java_hello/student/StudentController.java
@@ -3,7 +3,6 @@ package com.example.spring_java_hello.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
